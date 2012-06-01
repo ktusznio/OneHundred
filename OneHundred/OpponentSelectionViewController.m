de9aac98f68104e816503b90ab2df07488ec16cb
Kamil Tusznio <kamil.tusznio@gmail.com>
2012-06-01 17:37:22 -0400

    Powered the start button on the opponent selection screen.

--- OneHundred/OpponentSelectionViewController.m
+++ OneHundred/OpponentSelectionViewController.m
@@ -8,29 +8,32 @@
 
 #import "OpponentSelectionViewController.h"
 
+#import "AppDelegate.h"
+#import "BiddingViewController.h"
 #import "DumbComputerPlayer.h"
+#import "Game.h"
 #import "RandomComputerPlayer.h"
 
 static NSString *initialOpponentButtonText = @"Tap to add opponent";
 
 @implementation OpponentSelectionViewController
 
-@synthesize activePlayer, activatedOpponents, availableOpponents;
+@synthesize activatedOpponents, availableOpponents;
 @synthesize addFirstOpponentButton, addSecondOpponentButton, addThirdOpponentButton;
 
 - (id)init {
     self = [super init];
-    
+
     if (self) {
         [self setActivatedOpponents:[NSMutableArray array]];
-        
+
         // Set up the available opponents dictionary.
         NSArray *opponentClasses = [NSArray arrayWithObjects:[DumbComputerPlayer class], [RandomComputerPlayer class], nil];
         NSArray *opponentNames = [NSArray arrayWithObjects:@"Dumbox", @"WEPQUP", nil];
         [self setAvailableOpponents:[NSDictionary dictionaryWithObjects:opponentClasses
                                                                 forKeys:opponentNames]];
     }
-    
+
     return self;
 }
 
@@ -59,7 +62,7 @@ static NSString *initialOpponentButtonText = @"Tap to add opponent";
     UIButton *tappedButton = (UIButton *)sender;
     NSString *buttonText = [[tappedButton titleLabel] text];
     NSArray *opponentNames = [[self availableOpponents] allKeys];
-    
+
     if ([buttonText isEqualToString:initialOpponentButtonText]) {
         [tappedButton setTitle:[opponentNames objectAtIndex:0] forState:UIControlStateNormal];
     } else {
@@ -71,7 +74,7 @@ static NSString *initialOpponentButtonText = @"Tap to add opponent";
                 break;
             }
         }
-        
+
         // Select the next opponent or loop around to the first opponent if the last opponent is selected.
         if (selectedOpponentIndex == [opponentNames count] - 1) {
             [tappedButton setTitle:[opponentNames objectAtIndex:0] forState:UIControlStateNormal];
@@ -83,11 +86,38 @@ static NSString *initialOpponentButtonText = @"Tap to add opponent";
 }
 
 - (IBAction)onBackButtonTap:(id)sender {
-    
+
 }
 
 - (IBAction)onStartButtonTap:(id)sender {
-    
+    // Get the app delegate.
+    AppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+
+    // Create a new game and set the current game in the app delegate.
+    Game *currentGame = [[Game alloc] initWithTargetPoints:6 startingMoney:100];
+    [appDelegate setCurrentGame:currentGame];
+
+    // Add the active player to the game.
+    [currentGame addPlayer:[appDelegate activePlayer]];
+
+    // Loop over the add opponent buttons, adding the selected opponents to the game.
+    NSArray *addOpponentButtons = [NSArray arrayWithObjects:addFirstOpponentButton, addSecondOpponentButton, addThirdOpponentButton, nil];
+    for (UIButton *addOpponentButton in addOpponentButtons) {
+        NSString *opponentName = [[addOpponentButton titleLabel] text];
+        Class opponentClass = [availableOpponents objectForKey:opponentName];
+        if (opponentClass != nil) {
+            [currentGame addPlayer:[[opponentClass alloc] initWithName:opponentName]];
+        }
+    }
+
+    // Set the game's delegate to a bidding view controller and start the game.
+    BiddingViewController *biddingViewController = [[BiddingViewController alloc] init];
+    [currentGame setDelegate:biddingViewController];
+    [currentGame startGame];
+
+    // Push to the bidding screen.
+    UINavigationController *navigationController = [(AppDelegate *)[[UIApplication sharedApplication] delegate] navigationController];
+    [navigationController pushViewController:biddingViewController animated:YES];
 }
 
 @end
